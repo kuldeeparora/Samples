6160b665f35721aac1571d8bb0a7a5f68b428e32
Kuldeep Arora <kuldeep.arora@gmail.com>
2012-12-21 00:32:13 +0000

    Stop watch html5 & css3

---
+++ stop watch/js/clock.js
@@ -0,0 +1,65 @@
+$(function(){
+	var hours = $('.hoursCount>p');
+	var mins = $('.minutesCount>p');
+	var secs = $('.secondsCount>p');
+	var hCount = 1, mCount = 1, sCount = 1;
+	var stop = false;
+	var timer;
+
+    secs.bind('webkitAnimationEnd', function(){
+        this.style.webkitAnimationName = '';
+        console.log("webkitAnimationName end");
+    });
+    secs.addClass('flipup');
+
+    function updateDisplay(element, count){
+        element.addClass(function(){
+            if(count < 10){
+                element.html('0'+count);
+            }
+            else{
+                element.html(count);
+            }
+        });
+        secs.css('webkitAnimationName', 'flip1');
+        console.log('webkitAnimationName');
+        element.fadeTo('fast', 1, 0);
+    }
+
+	function updateClock(){
+		timer = setInterval(function(){
+			if(sCount < 60){
+				updateDisplay(secs, sCount++);
+        	}
+			else{
+				sCount = 0;
+				updateDisplay(secs, sCount++);
+				if(mCount < 60){
+					updateDisplay(mins, mCount++);
+				}
+				else{
+					mCount = 0;
+					updateDisplay(mins, mCount++);
+					updateDisplay(hours, hCount++);	
+				}
+			}
+		}, 1000);
+	}
+
+	$('.startButton').toggle(function(){
+		$('.startLabel>p').html('Stop');
+		updateClock();
+	}, function(){
+		$('.startLabel>p').html('Start');
+		clearInterval(timer);
+	});
+
+	$('.resetButton').click(function(){
+		sCount = mCount = hCount = 1;
+		updateDisplay(secs, 0);
+		updateDisplay(mins, 0);
+		updateDisplay(hours, 0);
+		$('.startLabel>p').html('Start');
+		clearInterval(timer);
+	});
+})
