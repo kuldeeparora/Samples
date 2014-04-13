3fdb77c325a79fa30973a23076a9c66c84367c39
Kuldeep <kuldeep.arora@gmail.com>
2014-04-13 02:02:36 +0100

    added basic level functionality and tried to remove node module

--- TV Guide/app/scripts/app.js
+++ TV Guide/app/scripts/app.js
@@ -1,9 +1,19 @@
 'use strict';
 
 angular
-  .module('tvGuideApp', [
+  .module('yApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
     'ngRoute'
-  ]);
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
--- TV Guide/app/scripts/controllers/DaytimeCtrl.js
+++ TV Guide/app/scripts/controllers/DaytimeCtrl.js
@@ -1,6 +1,6 @@
 (function(){
     'use strict';
-
+//    console.log(Demo.TvGuide.app);
     Demo.TvGuide.app.controller('DaytimeCtrl', function ($scope) {
         console.log('DayTimeCtrl');
         $scope.daytime = null;
