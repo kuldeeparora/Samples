f8a44792bc164224af13ac9adc63d858edb93236
Kuldeep <kuldeep.arora@gmail.com>
2014-04-14 16:14:50 +0100

    added nav functionality

--- TV Guide/app/scripts/controllers/DaytimeCtrl.js
+++ TV Guide/app/scripts/controllers/DaytimeCtrl.js
@@ -2,8 +2,29 @@
     'use strict';
 //    console.log(Demo.TvGuide.app);
     Demo.TvGuide.app.controller('DaytimeCtrl', function ($scope) {
-        console.log('DayTimeCtrl');
-        $scope.daytime = null;
+        var currentDate = new Date();
+        var weekday = new Array(7);
+            weekday[0] = "Sun";
+            weekday[1] = "Mon";
+            weekday[2] = "Tue";
+            weekday[3] = "Wed";
+            weekday[4] = "Thu";
+            weekday[5] = "Fri";
+            weekday[6] = "Sat";
+        var weekdayLength = weekday.length;
+
+        $scope.days = [];
+
+        $scope.days.push({day: "Today"});
+        currentDate.setDate(currentDate.getDate()+1)
+        for (var i = 1; i < weekdayLength; i++ ){
+            var day = weekday[currentDate.getDay()];
+            var date = currentDate.getDate()+"TH";
+            $scope.days.push({day: day, date: date});
+            currentDate.setDate(currentDate.getDate()+1)
+        }
+
+
     });
 
 }());
