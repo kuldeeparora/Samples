6ba4a01ab4f0ee3838740923fbc8f3bfb0e497c8
Kuldeep <kuldeep.arora@gmail.com>
2014-04-17 19:47:49 +0100

    backup of feature/tv_guide

--- TV Guide/app/scripts/controllers/ChannelFilterCtrl.js
+++ TV Guide/app/scripts/controllers/ChannelFilterCtrl.js
@@ -1,5 +1,12 @@
-'use strict';
-
-Demo.TvGuide.app.controller('ChannelFilterCtrl', function ($scope) {
-   
-  });
+(function () {
+    'use strict';
+    Demo.TvGuide.app.controller('ChannelFilterCtrl', function ($scope, ChannelDropdown) {
+        ChannelDropdown.getChannelDropdown().then(
+            function(dropdownValues){
+                $scope.categories = dropdownValues.categories;
+                $scope.packages = dropdownValues.packages;
+                $scope.cities = dropdownValues.cities;
+            }
+        );
+    });
+}());
