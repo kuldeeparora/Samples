26ddabd84a0171126d6f654121706e6d462ad862
Kuldeep <kuldeep.arora@gmail.com>
2014-04-23 01:18:08 +0100

    bower update, added time line

--- TV Guide/app/scripts/controllers/ChannelCtrl.js
+++ TV Guide/app/scripts/controllers/ChannelCtrl.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     Demo.TvGuide.app.controller('ChannelCtrl', function ($scope, EpgListing) {
-        EpgListing.getEpgListing('/json/21.json').then(function (epglisting) {
+        EpgListing.getEpgListing('/json/23.json').then(function (epglisting) {
             $scope.updateEpg(epglisting);
         });
 
@@ -21,23 +21,31 @@
                 $scope.category = $scope.themes[0];
                 $scope.package = $scope.packages[0];
             }
+//            $scope.findChannelWidth();
+        };
+
+        $scope.findChannelWidth = function(epgList, $filter){
+            var dateFilter = $filter('date');
+//            return dateFilter(new Date(), 'yyyy-MM-01');
+//            (epgList.end-epgList.start)*1000*(240/60);
+
         };
 
         $scope.checkFilter = function (channel) {
             return (($scope.category === channel.channelInfo.theme && $scope.package === channel.channelInfo.package) || ($scope.category === 'All Channels' && $scope.package === 'All Packages'));
         };
 
-        $scope.findMargin = function(){
+        $scope.findMarginPrevNext = function(){
             return parseInt($('.epg-shows-list').css('margin-left'));
         };
 
         $scope.prevEpisode = function(){
-            $scope.addMargin = $scope.findMargin();
+            $scope.addMargin = $scope.findMarginPrevNext();
             $('.epg-shows-list').css('margin-left', ($scope.addMargin + 100) + 'px');
         };
 
         $scope.nextEpisode = function(){
-            $scope.removeMargin = $scope.findMargin();
+            $scope.removeMargin = $scope.findMarginPrevNext();
             $('.epg-shows-list').css('margin-left', ($scope.removeMargin - 100) + 'px');
         };
 
