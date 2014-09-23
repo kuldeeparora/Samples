aa44125ba4a65cfa7f49d593a0b41228912c7c78
Kuldeep <kuldeep.arora@gmail.com>
2014-09-23 23:45:19 +0100

    added bootstrap validator on login & login setup

---
+++ williamslea/williamslea/_/js/src/login.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $('#loginForm').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            username: {
+                message: 'Please enter a valid user ID',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: 'The username can only consist of alphabetical, number and underscore'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter a valid password'
+                    }
+                }
+            }
+        }
+    })
+        .on('success.form.bv', function (e) {
+
+            // Prevent form submission
+            e.preventDefault();
+
+           console.log("tested");
+        });
+});
