962329685e993c95ba3b9177de64b96d20f3db0f
Kuldeep <kuldeep.arora@gmail.com>
2014-11-18 01:39:52 +0000

    yeoman phonegap & angularjs setup

---
+++ kyooboid/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kyooboidApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kyooboidApp
+ */
+angular.module('kyooboidApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
